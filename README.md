4dc4be3619ce651630d5b4f60200d77eb474d1fa
ruinofran <rubinofranciscodev@gmail.com>
2025-05-02 13:44:03 -0300

    Steps generator V3

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 Creado para la generación ágil de steps en la etapa de testeo para recetas con Geolocalización y otras
 
 # listado de steps contemplados y sus variables
-- wait: - (ver for y field)  
-- click: field / type (tipos text o raro)  
+- wait: time
+- wait field: field
+- click: field / type 
 - complete: field / value  
-- press: key / value (ver si tiene time o no, además tipos de key como Enter y ArrowDown)  
-- clickifexists: field  
-- reloadPage: sin argumentos  
-- execJS: code  
-- goto: value (ver url o paramUrl)  
+- press: key 
+- press many times: key / value
+- click if exists: field  
+- reload page: sin argumentos  
+- exec JS: code  
+- goto: value 
 - scroll: field / scrollIncrement  
--- generator.js
+++ generator.js
@@ -1,10 +1,13 @@
 /* ------------------------------------ */
-/* STEPS FUNCTIONS */
+/* STEPS FUNCTIONS (test logs: node .\generator.js) */
 
-// FALTA VARIANTE FIELD ------------------> {"fn":"wait","field":"div.vtex-login-2-x-emailPasswordOptionBtn"},
 const waitFN = (time) => `{"fn":"wait","for":${time}}`
 // EXAMPLE) time: 5000
 // console.log(waitFN(5000))
+
+const waitFieldFN = (field) => `{"fn":"wait","field":"${field}"}`
+// EXAMPLE) field: 'div[class*="myAccountWrapperLink"]'
+// console.log(waitFieldFN('div[class*="myAccountWrapperLink"]'))
 
 const clickFN = (field, type) => `{"fn":"click","field":"${field}","type":"${type}"}`
 // EXAMPLE) field: 'div[class*="myAccountWrapperLink"]', type: 'raro' (type: 'text' variable)
@@ -14,10 +17,13 @@ const completeFN = (field, value) => `{"fn":"complete","field":"${field}","value
 // EXAMPLE) field: 'div.vtex-login-2-x-inputContainerEmail input', value: 'email' 
 // console.log(completeFN('div.vtex-login-2-x-inputContainerEmail input', 'email'))
 
-// FALTA VARIANTE TIMES ------------------> {"fn":"press","key":"ArrowDown","value":"provincia_times"},
 const pressFN = (key) => `{"fn":"press","key":"${key}"}`
 // EXAMPLE) key: 'Enter' (key: 'ArrowDown' variable)
 // console.log(pressFN('Enter'))    
+
+const pressManyTimesFN = (key, value) => `{"fn":"press","key":"${key}","value":"${value}"}`
+// EXAMPLE) key: 'Enter', value: 'provincia_times' 
+// console.log(pressManyTimesFN('Enter', 'provincia_times'))
 
 const clickIfExistsFN = (field) => `{"fn":"clickifexists","field":"${field}"}`
 // EXAMPLE) field: '#onetrust-accept-btn-handler'
@@ -43,15 +49,17 @@ const scrollFN = (field, scrollIncrement) => `{"fn":"scroll","field":"${field}",
 /* OPTIONS */
 
 const stepTypes = [
-    { name: "wait", fn: waitFN, vars: ["time"] },
-    { name: "click", fn: clickFN, vars: ["field", "type"] },
-    { name: "complete", fn: completeFN, vars: ["field", "value"] },
-    { name: "press", fn: pressFN, vars: ["key"] },
-    { name: "clickifexists", fn: clickIfExistsFN, vars: ["field"] },
-    { name: "reloadPage", fn: reloadPageFN, vars: [] },
-    { name: "execJS", fn: execJSFN, vars: ["code"] },
-    { name: "goto", fn: gotoFN, vars: ["value"] },
-    { name: "scroll", fn: scrollFN, vars: ["field", "scrollIncrement"] }
+    { name: "wait", fn: waitFN, vars: ["time"], showTitle: false, title: "" },
+    { name: "wait field", fn: waitFieldFN, vars: ["field"], showTitle: false, title: "" },
+    { name: "click", fn: clickFN, vars: ["field", "type"], showTitle: true, title: "son válidos los types: text / raro" },
+    { name: "complete", fn: completeFN, vars: ["field", "value"], showTitle: false, title: "" },
+    { name: "press", fn: pressFN, vars: ["key"], showTitle: true, title: "algunos ejemplos de key: Enter / ArrowDown" },
+    { name: "press many times", fn: pressManyTimesFN, vars: ["key", "value"], showTitle: true, title: "algunos ejemplos de key: Enter / ArrowDown" },
+    { name: "click if exists", fn: clickIfExistsFN, vars: ["field"], showTitle: false, title: "" },
+    { name: "reload page", fn: reloadPageFN, vars: [], showTitle: false, title: "" },
+    { name: "exec JS", fn: execJSFN, vars: ["code"], showTitle: false, title: "" },
+    { name: "goto", fn: gotoFN, vars: ["value"], showTitle: false, title: "" },
+    { name: "scroll", fn: scrollFN, vars: ["field", "scrollIncrement"], showTitle: false, title: "" },
 ]
 
 /* ------------------------------------ */
@@ -60,20 +68,21 @@ const stepTypes = [
 // Associated with button "+" to add a new step
 const newStep = () => {
     
-    // 
+    // Getting html  
     let listContainer = document.querySelector("#stepList");
   
-    //
+    // Creating elements
     let newP = document.createElement("p");
     let newSelect = document.createElement("select");
     stepTypes.forEach(step => {
         let newOption = document.createElement("option");
         newOption.value = step.name;
         newOption.textContent = step.name;
+        if(step.showTitle) {
+            newOption.setAttribute("title", step.title);    
+        }
         newSelect.appendChild(newOption);
     });
-
-    //
     let configButton = document.createElement("button");
     configButton.textContent = "CONFIG";
     configButton.classList = "configButton";
@@ -83,20 +92,16 @@ const newStep = () => {
         let stepCode = step.fn(...step.vars.map(v => prompt(`Enter ${v}:`)));
         contentSpan.textContent = stepCode;
     };
-
-    //
     let deleteButton = document.createElement("button");
     deleteButton.textContent = "X";
     deleteButton.classList = "deleteButton";
     deleteButton.onclick = () => {
         listContainer.removeChild(newP);
     };
-
-    //
     let contentSpan = document.createElement("span");
     contentSpan.textContent = "{ CONFIG AND SHOW STEP }";
     
-    //
+    // Modifying the dom
     newP.appendChild(newSelect);
     newP.appendChild(configButton);
     newP.appendChild(contentSpan);  
@@ -109,8 +114,12 @@ let formattedResultStep = "";
 
 // Associated with button "GENERATE" to generate the result
 const showResult = () => {
+    
+    // Getting html  
     let result = document.querySelector("#resultSteps");
     let formattedResult = document.querySelector("#formattedResultSteps");
+    
+    // Processed
     let steps = document.querySelectorAll("span");
     if(steps.length > 0) {
         resultStep = "["; 
